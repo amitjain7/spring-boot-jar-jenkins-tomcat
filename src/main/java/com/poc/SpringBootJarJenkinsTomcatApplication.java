57f5225ee1aeafd21251bbb48e3ad7640d27a0fc
Amit Jain <amijain0@publicisgroupe.net>
2021-06-12 21:57:15 +0530

    Initial project

---
+++ src/main/java/com/poc/SpringBootJarJenkinsTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJarJenkinsTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJarJenkinsTomcatApplication.class, args);
+	}
+
+}
